6cc954750c7a0448911608e9af0cd78bded8d117
Leonardo Melgar <leonardomelgar1@gmail.com>
2022-08-20 20:04:25 -0300

    Ventana principal creada.

---
+++ Tarea1/src/Presentacion/Principal.java
@@ -0,0 +1,68 @@
+package Presentacion;
+
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+/**
+* Clase principal (Frame) con el método Main.
+* @author leonardo.melgar
+*
+*/
+
+public class Principal {
+	private JFrame frmReservas_UY;
+	
+	
+	
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    Principal window = new Principal();
+                    window.frmReservas_UY.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+    
+    public Principal() {
+    	initialize();
+    }
+    
+    private void initialize() {
+        
+        // Se crea el Frame con las dimensiones indicadas.
+        frmReservas_UY = new JFrame();
+        frmReservas_UY.setTitle("Reservas_UY admin");
+        frmReservas_UY.setBounds(100, 100, 450, 400);
+        frmReservas_UY.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Se crea una barra de menú (JMenuBar) con dos menú (JMenu) desplegables.
+        // Cada menú contiene diferentes opciones (JMenuItem), los cuales tienen un 
+        // evento asociado que permite realizar una acción una vez se seleccionan. 
+        JMenuBar menuBar = new JMenuBar();
+        frmReservas_UY.setJMenuBar(menuBar);
+
+        JMenu menuSistema = new JMenu("Sistema");
+        menuBar.add(menuSistema);
+
+        JMenuItem menuSalir = new JMenuItem("Salir");
+        menuSalir.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+                // Salgo de la aplicación
+                frmReservas_UY.setVisible(false);
+                frmReservas_UY.dispose();
+            }
+        });
+        menuSistema.add(menuSalir);
+    }
+
+}
