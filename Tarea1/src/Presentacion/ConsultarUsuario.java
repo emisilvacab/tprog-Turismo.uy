276097a7ab2a9485ccc3e14a3d2e10c6ee205e36
Joaquin Vidal <joaquin.vidal@fing.edu.uy>
2022-08-21 20:43:19 -0300

    falta parte final consulta de ususario

--- Tarea1/src/Presentacion/ConsultarUsuario.java
+++ Tarea1/src/Presentacion/ConsultarUsuario.java
@@ -7,6 +7,7 @@ import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 import excepciones.usuarioNoExisteException;
+import logica.DataType.DataTurista;
 import logica.DataType.DataUsuario;
 import logica.controladores.IControladorUsuario;
 
@@ -24,6 +25,7 @@ import javax.swing.JList;
 import javax.swing.SwingConstants;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
+import java.awt.Color;
 
 public class ConsultarUsuario extends JInternalFrame {
 	/**
@@ -42,6 +44,9 @@ public class ConsultarUsuario extends JInternalFrame {
 	private JLabel nicknameTag;
 	private JLabel nombreTag;
 	private JLabel apellidoTag;
+	private JLabel correoTag;
+	private JLabel nacimientoTag;
+	private JLabel tipoUsuario;
 
 	public ConsultarUsuario(IControladorUsuario icu) {
 		contUser = icu;
@@ -52,14 +57,14 @@ public class ConsultarUsuario extends JInternalFrame {
         setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
         setClosable(true);
         setTitle("Consultar un Usuario");
-        setBounds(30, 30, 400, 280);
+        setBounds(30, 30, 400, 350);
 		
 		Set<String> ciUsers = contUser.obtenerUsuarios();
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{303, 0, 73, 0};
-		gridBagLayout.rowHeights = new int[]{27, 0, 0, 0, 0, 178, 29, 0};
-		gridBagLayout.columnWeights = new double[]{1.0, 0.0, 0.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.columnWidths = new int[]{303, 0, 0, 73, 0};
+		gridBagLayout.rowHeights = new int[]{27, 0, 0, 0, 0, 0, 0, 0, 178, 29, 0};
+		gridBagLayout.columnWeights = new double[]{1.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
 		getContentPane().setLayout(gridBagLayout);
 		//		Set<String> ciUsers = new HashSet<String>();
 		//		ciUsers.add("joaquin");
@@ -73,9 +78,8 @@ public class ConsultarUsuario extends JInternalFrame {
 				}
 				GridBagConstraints gbc_listaUsuarios = new GridBagConstraints();
 				gbc_listaUsuarios.anchor = GridBagConstraints.NORTH;
-				gbc_listaUsuarios.fill = GridBagConstraints.HORIZONTAL;
-				gbc_listaUsuarios.insets = new Insets(0, 0, 5, 0);
-				gbc_listaUsuarios.gridwidth = 3;
+				gbc_listaUsuarios.insets = new Insets(0, 0, 5, 5);
+				gbc_listaUsuarios.gridwidth = 4;
 				gbc_listaUsuarios.gridx = 0;
 				gbc_listaUsuarios.gridy = 0;
 				getContentPane().add(listaUsuarios, gbc_listaUsuarios);
@@ -101,11 +105,12 @@ public class ConsultarUsuario extends JInternalFrame {
 				});
 		    
 		    JLabel lblInfoUsuario = new JLabel("Información de Usuario");
+		    lblInfoUsuario.setForeground(Color.BLUE);
 		    lblInfoUsuario.setVerticalAlignment(SwingConstants.TOP);
 		    lblInfoUsuario.setBounds(104, 70, 180, 14);
 		    GridBagConstraints gbc_lblInfoUsuario = new GridBagConstraints();
 		    gbc_lblInfoUsuario.anchor = GridBagConstraints.NORTH;
-		    gbc_lblInfoUsuario.gridwidth = 4;
+		    gbc_lblInfoUsuario.gridwidth = 5;
 		    gbc_lblInfoUsuario.insets = new Insets(0, 0, 5, 0);
 		    gbc_lblInfoUsuario.gridx = 0;
 		    gbc_lblInfoUsuario.gridy = 1;
@@ -124,7 +129,7 @@ public class ConsultarUsuario extends JInternalFrame {
 		    GridBagConstraints gbc_nicknameUsuario = new GridBagConstraints();
 		    gbc_nicknameUsuario.fill = GridBagConstraints.BOTH;
 		    gbc_nicknameUsuario.insets = new Insets(0, 0, 5, 5);
-		    gbc_nicknameUsuario.gridx = 1;
+		    gbc_nicknameUsuario.gridx = 2;
 		    gbc_nicknameUsuario.gridy = 2;
 		    getContentPane().add(nicknameUsuario, gbc_nicknameUsuario);
 		
@@ -153,7 +158,7 @@ public class ConsultarUsuario extends JInternalFrame {
 		GridBagConstraints gbc_nombreUsuario = new GridBagConstraints();
 		gbc_nombreUsuario.anchor = GridBagConstraints.WEST;
 		gbc_nombreUsuario.insets = new Insets(0, 0, 5, 5);
-		gbc_nombreUsuario.gridx = 1;
+		gbc_nombreUsuario.gridx = 2;
 		gbc_nombreUsuario.gridy = 3;
 		getContentPane().add(nombreUsuario, gbc_nombreUsuario);
 		
@@ -170,16 +175,67 @@ public class ConsultarUsuario extends JInternalFrame {
 		GridBagConstraints gbc_apellidoUsuario = new GridBagConstraints();
 		gbc_apellidoUsuario.fill = GridBagConstraints.BOTH;
 		gbc_apellidoUsuario.insets = new Insets(0, 0, 5, 5);
-		gbc_apellidoUsuario.gridx = 1;
+		gbc_apellidoUsuario.gridx = 2;
 		gbc_apellidoUsuario.gridy = 4;
 		getContentPane().add(apellidoUsuario, gbc_apellidoUsuario);
+		
+		correoTag = new JLabel("Correo: ");
+		GridBagConstraints gbc_correoTag = new GridBagConstraints();
+		gbc_correoTag.insets = new Insets(0, 0, 5, 5);
+		gbc_correoTag.gridx = 0;
+		gbc_correoTag.gridy = 5;
+		getContentPane().add(correoTag, gbc_correoTag);
+		
+		correoUsuario = new JLabel(userSelected.getCorreo());
+		correoUsuario.setBounds(10, 114, 65, 14);
+		GridBagConstraints gbc_correoUsuario = new GridBagConstraints();
+		gbc_correoUsuario.insets = new Insets(0, 0, 5, 5);
+		gbc_correoUsuario.gridx = 2;
+		gbc_correoUsuario.gridy = 5;
+		getContentPane().add(correoUsuario, gbc_correoUsuario);
+		
+		nacimientoTag = new JLabel("Fecha de nacimiento: ");
+		GridBagConstraints gbc_nacimientoTag = new GridBagConstraints();
+		gbc_nacimientoTag.insets = new Insets(0, 0, 5, 5);
+		gbc_nacimientoTag.gridx = 0;
+		gbc_nacimientoTag.gridy = 6;
+		getContentPane().add(nacimientoTag, gbc_nacimientoTag);
+		
+		nacimientoUsuario = new JLabel(userSelected.getNacimiento().toString());
+		GridBagConstraints gbc_nacimientoUsuario = new GridBagConstraints();
+		gbc_nacimientoUsuario.insets = new Insets(0, 0, 5, 5);
+		gbc_nacimientoUsuario.gridx = 2;
+		gbc_nacimientoUsuario.gridy = 6;
+		getContentPane().add(nacimientoUsuario, gbc_nacimientoUsuario);
+		
+		tipoUsuario = new JLabel();
+		GridBagConstraints gbc_tipoUsuario = new GridBagConstraints();
+		gbc_tipoUsuario.insets = new Insets(0, 0, 5, 5);
+		gbc_tipoUsuario.gridx = 1;
+		gbc_tipoUsuario.gridy = 7;
+		getContentPane().add(tipoUsuario, gbc_tipoUsuario);
+		tipoUsuario.setVisible(false);
+		
+		if (userSelected.getClass() == DataTurista.class) {
+			tipoUsuario.setText("El usuario es un Turista");
+			tipoUsuario.setVisible(true);
+			
+			
+		}
+		
 		GridBagConstraints gbc_btnCerrar = new GridBagConstraints();
+		gbc_btnCerrar.insets = new Insets(0, 0, 0, 5);
 		gbc_btnCerrar.anchor = GridBagConstraints.NORTH;
 		gbc_btnCerrar.fill = GridBagConstraints.HORIZONTAL;
-		gbc_btnCerrar.gridwidth = 3;
+		gbc_btnCerrar.gridwidth = 4;
 		gbc_btnCerrar.gridx = 0;
-		gbc_btnCerrar.gridy = 6;
+		gbc_btnCerrar.gridy = 9;
 		getContentPane().add(btnCerrar, gbc_btnCerrar);
+		
+		
+		
+		
+		
 		
 	
         
