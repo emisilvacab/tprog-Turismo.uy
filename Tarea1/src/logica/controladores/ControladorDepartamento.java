d13aa1c067a3f31cba789e4c7fb3a28f03e20bff
Leonardo Melgar <leonardomelgar1@gmail.com>
2022-08-22 16:45:41 -0300

    Se agregó la operacion obtenerDepartamentos()

--- Tarea1/src/logica/controladores/ControladorDepartamento.java
+++ Tarea1/src/logica/controladores/ControladorDepartamento.java
@@ -1,8 +1,21 @@
 package logica.controladores;
 
-public class ControladorDepartamento implements IControladorDepartamento {
-	public ControladorDepartamento() {
-		
-	}
+import java.util.HashMap;
+import java.util.HashSet;
 
+import data.ManejadorDepartamento;
+import logica.Departamento;
+
+public class ControladorDepartamento implements IControladorDepartamento {
+	
+	public ControladorDepartamento() {}
+	public HashSet<String> obtenerDepartamentos(){
+		ManejadorDepartamento mDptos = ManejadorDepartamento.getInstance();
+		HashMap<String, Departamento> dptos = mDptos.getDepartamentos();
+		HashSet<String> res = new HashSet<String>();
+		dptos.forEach((key,value)->{
+			res.add(key);
+		});
+		return res;
+	};
 }
