03e59b23dd669f15a9fc943480082c7e98072ebb
Emiliano Silva <emisilvacab@gmail.com>
2022-08-29 22:40:46 -0300

    borrado de clases

--- Tarea1/src/logica/controladores/IControladorPaquete.java
+++
@@ -1,9 +0,0 @@
-package logica.controladores;
-
-import java.util.Set;
-
-public interface IControladorPaquete {
-	public abstract String[] mostrarSalidasAsociadas(String[] actividadesOfrecidas);
-
-}
-
