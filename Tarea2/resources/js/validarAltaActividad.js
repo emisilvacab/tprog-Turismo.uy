28a589859dd032ea1380b1408acdd6aeb44daaf0
Emiliano Silva <emisilvacab@gmail.com>
2022-09-16 14:30:28 -0300

    altaSalida y altaActividad

---
+++ Tarea2/resources/js/validarAltaActividad.js
@@ -0,0 +1,45 @@
+function validarAltaActividad(){
+	
+	if (document.getElementById('depAct').value.length == 0){
+		alert('Seleccione un departamento');
+    	return false;
+    }
+    else if(document.getElementById('nombreAct').value.length == 0 ){
+		alert('Ingrese un nombre para la actividad');
+    	return false;
+	}
+	/*se va a cambiar cuando sea dinamico*/
+    else if(document.getElementById('nombreAct').value == "Degusta" || document.getElementById('nombreAct').value == "Teatro con Sabores"){
+		alert('Ya existe una actividad con ese nombre');
+    	return false;
+	}
+    else if(document.getElementById('descripcionAct').value.length == 0 ){
+		alert('Ingrese una descripción');
+    	return false;
+	}
+    else if(document.getElementById('durAct').value.length == 0 ){
+		alert('Ingrese una duración');
+    	return false;
+	}
+    else if(document.getElementById('durAct').value <= 0 ){
+		alert('Ingrese una duración mayor que 0');
+    	return false;
+	}
+    else if(document.getElementById('costoAct').value.length == 0 ){
+		alert('Ingrese un costo');
+    	return false;
+	}
+    else if(document.getElementById('costoAct').value < 0 ){
+		alert('Ingrese un costo mayor o igual a 0');
+    	return false;
+	}
+    else if(document.getElementById('ciudadAct').value.length == 0 ){
+		alert('Ingrese una ciudad');
+    	return false;
+	}
+    else if(document.getElementById('catAct').value.length == 0 ){
+		alert('Seleccione categoría(s)');
+    	return false;
+	}
+	return true;
+}
